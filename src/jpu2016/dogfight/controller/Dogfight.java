ea4d05cb9d0121335dd5aedd75583928a1ec32c7
CharlyPey <charly.peyssard@viacesi.fr>
2017-06-02 11:14:31 +0200

    Main

--- src/jpu2016/dogfight/controller/Dogfight.java
+++ src/jpu2016/dogfight/controller/Dogfight.java
@@ -1,9 +1,15 @@
 package jpu2016.dogfight.controller;
 
+import jpu2016.dogfight.model.DogfightModel;
+import jpu2016.dogfight.view.DogfightView;
+
 public class Dogfight {
 
 	public static void main(final String[] args) {
-		final DogfightController dogfightController = new DogfightController(null);
+		final DogfightModel dogfightModel = new DogfightModel();
+		final DogfightController dogfightController = new DogfightController(dogfightModel);
+		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel, dogfightModel);
+		dogfightController.setViewSystem(dogfightView);
 		dogfightController.play();
 	}
 
