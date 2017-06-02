fb9b9911fa7398528b4d2d35a4c5331b061d75c3
C-Benjamin <benjamin.cuny@viacesi.fr>
2017-06-02 10:10:05 +0200

    Phase 3 1009

---
+++ src/jpu2016/dogfight/controller/Dogfight.java
@@ -0,0 +1,10 @@
+package jpu2016.dogfight.controller;
+
+public class Dogfight {
+
+	public static void main(final String[] args) {
+		final DogfightController dogfightController = new DogfightController(null);
+		dogfightController.play();
+	}
+
+}
