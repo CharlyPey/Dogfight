4b7c7a6a53061c021c896aba7967671aa80ed71f
C-Benjamin <benjamin.cuny@viacesi.fr>
2017-06-03 17:21:59 +0200

    4

--- src/jpu2016/dogfight/view/EventPerformer.java
+++ src/jpu2016/dogfight/view/EventPerformer.java
@@ -8,8 +8,7 @@ import jpu2016.dogfight.controller.UserOrder;
 import jpu2016.gameframe.IEventPerformer;
 
 public class EventPerformer implements IEventPerformer {
-	private final IOrderPerformer	orderPerformer;
-	private UserOrder				userOrder;
+	private final IOrderPerformer orderPerformer;
 
 	public EventPerformer(final IOrderPerformer orderPerformer) {
 		this.orderPerformer = orderPerformer;
@@ -18,27 +17,45 @@ public class EventPerformer implements IEventPerformer {
 
 	@Override
 	public void eventPerform(final KeyEvent keyCode) {
-		this.keyCodeToUserOrder(keyCode.getKeyCode());
+		this.orderPerformer.orderPerform(this.keyCodeToUserOrder(keyCode.getKeyCode()));
 	}
 
 	private UserOrder keyCodeToUserOrder(final int keyCode) {
+		final UserOrder userOrder;
 		switch (keyCode) {
 		case KeyEvent.VK_UP:
-			this.orderPerformer.orderPerform(new UserOrder(0, Order.UP));
+			userOrder = new UserOrder(0, Order.UP);
 			break;
 		case KeyEvent.VK_DOWN:
-			this.orderPerformer.orderPerform(new UserOrder(0, Order.DOWN));
+			userOrder = new UserOrder(0, Order.DOWN);
 			break;
 		case KeyEvent.VK_LEFT:
-			this.orderPerformer.orderPerform(new UserOrder(0, Order.LEFT));
+			userOrder = new UserOrder(0, Order.LEFT);
 			break;
 		case KeyEvent.VK_RIGHT:
-			this.orderPerformer.orderPerform(new UserOrder(0, Order.RIGHT));
+			userOrder = new UserOrder(0, Order.RIGHT);
 			break;
-		case KeyEvent.VK_SPACE:
-			this.orderPerformer.orderPerform(new UserOrder(0, Order.SHOOT));
+		case KeyEvent.VK_CONTROL:
+			userOrder = new UserOrder(0, Order.SHOOT);
 			break;
+		case KeyEvent.VK_Z:
+			userOrder = new UserOrder(1, Order.UP);
+			break;
+		case KeyEvent.VK_S:
+			userOrder = new UserOrder(1, Order.DOWN);
+			break;
+		case KeyEvent.VK_Q:
+			userOrder = new UserOrder(1, Order.LEFT);
+			break;
+		case KeyEvent.VK_D:
+			userOrder = new UserOrder(1, Order.RIGHT);
+			break;
+		case KeyEvent.VK_SHIFT:
+			userOrder = new UserOrder(1, Order.SHOOT);
+			break;
+		default:
+			return null;
 		}
-		return null;
+		return userOrder;
 	}
 }
