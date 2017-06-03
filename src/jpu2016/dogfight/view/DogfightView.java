b2d1b0b435e04cf75e712a55f6cf63bc10ceedf4
C-Benjamin <benjamin.cuny@viacesi.fr>
2017-06-03 18:52:34 +0200

    4

--- src/jpu2016/dogfight/view/DogfightView.java
+++ src/jpu2016/dogfight/view/DogfightView.java
@@ -2,6 +2,9 @@ package jpu2016.dogfight.view;
 
 import java.util.Observable;
 
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
 import jpu2016.dogfight.controller.IOrderPerformer;
 import jpu2016.dogfight.model.IDogfightModel;
 import jpu2016.gameframe.GameFrame;
@@ -10,28 +13,30 @@ public class DogfightView implements IViewSystem, Runnable {
 
 	private final EventPerformer	eventPerformer;
 	private final GraphicsBuilder	graphicsBuilder;
-	private final GameFrame			gameFrame;
+	private final Observable		observable;
+	private GameFrame				gameFrame;
 
 	public DogfightView(final IOrderPerformer orderPerformer, final IDogfightModel dogfightModel,
 			final Observable observable) {
 		this.eventPerformer = new EventPerformer(orderPerformer);
 		this.graphicsBuilder = new GraphicsBuilder(dogfightModel);
-		this.gameFrame = new GameFrame("Dogfight", this.eventPerformer, this.graphicsBuilder, observable);
+		this.observable = observable;
+		SwingUtilities.invokeLater(this);
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		this.gameFrame = new GameFrame("Dogfight", this.eventPerformer, this.graphicsBuilder, this.observable);
 
 	}
 
 	@Override
 	public void displayMessage(final String message) {
-
+		JOptionPane.showMessageDialog(null, message);
 	}
 
 	@Override
 	public void closeAll() {
-
+		this.gameFrame.dispose();
 	}
 }
